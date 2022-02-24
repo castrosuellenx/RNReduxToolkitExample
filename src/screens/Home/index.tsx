887cb23c547ce78622897540117109ffc10f8d22
Suellen Castro <castrosuellenx@gmail.com>
2022-02-24 16:49:05 -0300

    add styles on Home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,63 @@
 import React from 'react';
+import { ActivityIndicator } from 'react-native';
+import { useTheme } from 'styled-components/native';
 
 import * as S from './styles';
 
 const Home: React.FC = () => {
+  const theme = useTheme();
+
+  const loading = false; // PROVISORIO
+
   return (
     <S.Container>
-      <S.Title>Home</S.Title>
+      <S.StatusBar
+        barStyle={theme.colors.barStyle}
+        backgroundColor={theme.colors.background.main}
+      />
+      <S.Title>Solar System</S.Title>
+
+      <S.Box>
+        <S.Subtitle>Planet</S.Subtitle>
+
+        <S.Content>
+          <S.WrapperInfo>
+            <S.InfoTitle>Name</S.InfoTitle>
+            <S.InfoSubtitle>La Terre</S.InfoSubtitle>
+          </S.WrapperInfo>
+
+          <S.WrapperInfo>
+            <S.InfoTitle>English Name</S.InfoTitle>
+            <S.InfoSubtitle>Earth</S.InfoSubtitle>
+          </S.WrapperInfo>
+
+          <S.WrapperInfo>
+            <S.InfoTitle>Moons</S.InfoTitle>
+            <S.InfoSubtitle>La Lune - Moon</S.InfoSubtitle>
+          </S.WrapperInfo>
+
+          <S.WrapperInfo>
+            <S.InfoTitle>Density</S.InfoTitle>
+            <S.InfoSubtitle>5.5136</S.InfoSubtitle>
+          </S.WrapperInfo>
+
+          <S.WrapperInfo>
+            <S.InfoTitle>Gravity</S.InfoTitle>
+            <S.InfoSubtitle>9.8</S.InfoSubtitle>
+          </S.WrapperInfo>
+        </S.Content>
+
+        <S.Button activeOpacity={0.7}>
+          {loading ? (
+            <ActivityIndicator
+              size="small"
+              color={theme.colors.text.secondary}
+            />
+          ) : (
+            <S.ButtonTitle>Explore a new Planet!</S.ButtonTitle>
+          )}
+        </S.Button>
+      </S.Box>
     </S.Container>
   );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,7 +1,77 @@
 import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { TouchableOpacityProps } from 'react-native';
 
 export const Container = styled.View`
   flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.background.main};
 `;
 
-export const Title = styled.Text``;
+export const StatusBar = styled.StatusBar``;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fontFamily.bold};
+  font-size: ${RFValue(30)}px;
+  color: ${({ theme }) => theme.colors.text.primary};
+  margin-bottom: ${RFValue(25)}px;
+`;
+
+export const Box = styled.View`
+  width: 90%;
+  align-items: center;
+  border-radius: ${RFValue(20)}px;
+  padding-vertical: ${RFValue(25)}px;
+  background-color: ${({ theme }) => theme.colors.background.medium};
+`;
+
+export const Subtitle = styled.Text`
+  font-family: ${({ theme }) => theme.fontFamily.bold};
+  font-size: ${RFValue(22)}px;
+  color: ${({ theme }) => theme.colors.text.primary};
+  margin-bottom: ${RFValue(15)}px;
+`;
+
+export const Content = styled.View`
+  width: 85%;
+  border-radius: ${RFValue(10)}px;
+  padding-vertical: ${RFValue(15)}px;
+  background-color: ${({ theme }) => theme.colors.background.light};
+`;
+
+export const WrapperInfo = styled.View`
+  margin-vertical: ${RFValue(10)}px;
+`;
+
+export const InfoTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fontFamily.bold};
+  font-size: ${RFValue(16)}px;
+  color: ${({ theme }) => theme.colors.text.primary};
+  margin-left: ${RFValue(25)}px;
+  margin-bottom: ${RFValue(5)}px;
+`;
+
+export const InfoSubtitle = styled.Text`
+  font-family: ${({ theme }) => theme.fontFamily.regular};
+  font-size: ${RFValue(14)}px;
+  color: ${({ theme }) => theme.colors.text.primary};
+  margin-left: ${RFValue(25)}px;
+`;
+
+export const Button = styled.TouchableOpacity`
+  width: 60%;
+  align-items: center;
+  justify-content: center;
+  align-self: center;
+  border-radius: ${RFValue(10)}px;
+  padding-vertical: ${RFValue(15)}px;
+  margin-top: ${RFValue(30)}px;
+  background-color: ${({ theme }) => theme.colors.button};
+`;
+
+export const ButtonTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fontFamily.bold};
+  font-size: ${RFValue(16)}px;
+  color: ${({ theme }) => theme.colors.text.secondary};
+`;
