c117b27f418af80db0164f69c86a0c5ff7a854db
Suellen Castro <castrosuellenx@gmail.com>
2022-02-24 14:13:42 -0300

    add config styles theme and assets

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,23 @@
+import { StatusBarStyle } from 'react-native';
+
+export default {
+  title: 'dark',
+
+  colors: {
+    primaryMain: '#020524',
+    primaryMedium: '#08094A',
+    primaryLight: '#240f5A',
+
+    button: '#F1A208',
+
+    text: '#FEFCFB',
+    textSecondary: '#121717',
+
+    barStyle: 'light-content' as StatusBarStyle | null | undefined,
+  },
+
+  fontFamily: {
+    regular: 'Leelawadee',
+    bold: 'Leelawadee Bold',
+  },
+};
