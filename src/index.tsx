71ffdb4ccf699c7873a96b5143e852ece52ef4b6
Suellen Castro <castrosuellenx@gmail.com>
2022-02-26 18:16:38 -0300

    update src index

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components/native';
-import Counter from './screens/Counter';
 
 // import SolarSystem from './screens/SolarSystem';
+// import Counter from './screens/Counter';
+import User from './screens/User';
 import store from './store';
 import { dark } from './styles/themes';
 
@@ -12,7 +13,8 @@ const App: React.FC = () => {
     <Provider store={store}>
       <ThemeProvider theme={dark}>
         {/* <SolarSystem /> */}
-        <Counter />
+        {/* <Counter /> */}
+        <User />
       </ThemeProvider>
     </Provider>
   );
