300e57c14b51f32e32b18bf0ddfa4dc6db0357ac
Suellen Castro <castrosuellenx@gmail.com>
2022-02-26 18:16:27 -0300

    update redux store

---
+++ src/store/index.ts
@@ -0,0 +1,22 @@
+import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux';
+
+import planetsReducer from './slices/planetsSlice';
+import counterReducer from './slices/counterSlice';
+import userReducer from './slices/userSlice';
+
+const store = configureStore({
+  reducer: {
+    planets: planetsReducer,
+    counter: counterReducer,
+    user: userReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
+// export type AppThunk = ThunkAction<void, RootState, null, Action<string>>;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+
+export default store;
